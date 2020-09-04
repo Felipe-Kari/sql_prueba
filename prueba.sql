905a285f2b01ebc9123ee86a2d539c09c4f09e06
Felipe Kari <felipe.kari@hotmail.com>
2020-09-04 13:04:35 -0400

    initial commit

---
+++ prueba.sql
@@ -0,0 +1,15 @@
+-- Crear Modelo en DB
+
+--Insertar 5 Clientes
+
+--Insertar 8 productos
+
+--Insertar 3 categorias
+
+--Insertar 10 facturas
+
+--¿Que cliente realizó la compra más cara?
+
+--¿Que cliente pagó sobre 100 de monto?
+
+--¿Cuantos clientes han comprado el producto 6
